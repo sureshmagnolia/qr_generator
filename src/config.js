c7668cdc6d39e623a1badeb821d37c8730df8248
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2026-02-22 20:15:49 +0530

    Update GOOGLE_SHEET_URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTuxbNh5Y8UYVwMR752Z5rqNcPAvdzrUvfuAr_ifFcXt2UjQZ5K4xzt0jhHUd3hGFNbUtPpqSmXDxym/pub?gid=497369614&single=true&output=csv',
+    GOOGLE_SHEET_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTeMmrTLZE-JaNh3hAce6a7fhXA65PcPyx62N-Tk-4uQwW93gD8jx3T9iC8exUleEtzcuz3sjlkhAk9/pub?gid=497369614&single=true&output=csv',
     MAX_LABELS: 20
 };
